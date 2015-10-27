4b679e1487bc31e25a44d1f918a80e0839755441
Samuel Brynolf <samuel.brynolf@aventyret.com>
2015-10-27 13:39:55 +0100

    v1

--- mis_includes/mis_scripts/mis_popup.js
+++ mis_includes/mis_scripts/mis_popup.js
@@ -6,20 +6,9 @@
     var popup_src = $('.mis_popup'),
         body = $('body'),
         overlay_ID = 'mis_overlay',
-        clones_exist = false,
         imgcloned_class = 'is-cloned',
         new_sizes_attr = '100vw',
         show_class = 's-is-visible';
-
-    function create_popimg($this){
-        var popped_img = $this;
-        var popped_id = popped_img.attr('data-misid');
-
-        popped_img.clone()
-            .addClass(imgcloned_class)
-            .attr('sizes', new_sizes_attr)
-            .attr('id', popped_id).appendTo(body);
-    }
 
     if(popup_src.length){
         // Create and append overlay
@@ -27,35 +16,33 @@
         overlay.setAttribute('id', overlay_ID);
         document.body.appendChild(overlay);
         overlay = $('#'+overlay_ID); // Create jQuery object
+
+        // Create popup-clones
+        popup_src.each(function(){
+            var popped_img = $(this);
+            var popped_id = popped_img.attr('data-misid');
+
+            popped_img.clone()
+                .addClass(imgcloned_class)
+                .attr('sizes', new_sizes_attr)
+                .attr('id', popped_id).appendTo(body);
+        });
+        var clones = $('.'+imgcloned_class);
     }
 
     popup_src.on('click', function(e){
         var $this = $(this);
-        var popped_id = $this.attr('data-misid');
-
-        if(!clones_exist){
-            create_popimg($this);
-        }
+        var popped_img = $('#'+ $this.attr('data-misid'));
 
         overlay.addClass(show_class);
-        $('#'+popped_id).addClass(show_class);
+        popped_img.addClass(show_class);
 
         e.preventDefault(e);
     });
 
-    body.on('click', '#'+overlay_ID+', .'+imgcloned_class, function(e){
-
-        if(!clones_exist){
-            $('.'+imgcloned_class).remove();
-            popup_src.each(function(){
-                create_popimg($(this));
-                clones_exist = true;
-                clones = $('.'+imgcloned_class);
-            });
-        } else {
-            clones.removeClass(show_class);
-        }
-
+    //body.on('click', '#'+overlay_ID+', .'+imgcloned_class, function(e){
+    overlay.add(clones).on('click', function(e){
+        clones.removeClass(show_class);
         overlay.removeClass(show_class);
 
         e.stopPropagation();
