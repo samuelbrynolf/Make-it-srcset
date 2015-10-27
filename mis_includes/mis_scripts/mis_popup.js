04fa7e5743b92f93f177da6418c0c506c87e5779
Samuel Brynolf <samuel.brynolf@aventyret.com>
2015-10-27 12:56:47 +0100

    Gallery solution done

--- mis_includes/mis_scripts/mis_popup.js
+++ mis_includes/mis_scripts/mis_popup.js
@@ -3,26 +3,25 @@
 
 (function($, document) {
 
-    var popup_trigger = $('.mis_container'),
+    var popup_src = $('.mis_img'),
         body = $('body'),
-        items_cloned = false,
-        new_sizes = '100vw',
-        show_class = 's-is-visible',
+        overlay_ID = 'mis_overlay',
+        clones_exist = false,
         imgcloned_class = 'is-cloned',
-        overlay_ID = 'mis_overlay';
-
+        new_sizes_attr = '100vw',
+        show_class = 's-is-visible';
 
     function create_popimg($this){
-        var popped_img = $this.find('img');
+        var popped_img = $this;
         var popped_id = popped_img.attr('data-misid');
 
         popped_img.clone()
             .addClass(imgcloned_class)
-            .attr('sizes', new_sizes)
+            .attr('sizes', new_sizes_attr)
             .attr('id', popped_id).appendTo(body);
     }
 
-    if(popup_trigger.length){
+    if(popup_src.length){
         // Create and append overlay
         var overlay = document.createElement('div');
         overlay.setAttribute('id', overlay_ID);
@@ -30,30 +29,35 @@
         overlay = $('#'+overlay_ID); // Create jQuery object
     }
 
-    popup_trigger.on('click', function(e){
+    popup_src.on('click', function(e){
         var $this = $(this);
+        var popped_id = $this.attr('data-misid');
 
-        overlay.addClass(show_class);
-
-        if(!items_cloned){
+        if(!clones_exist){
             create_popimg($this);
         }
+
+        overlay.addClass(show_class);
+        $('#'+popped_id).addClass(show_class);
 
         e.preventDefault(e);
     });
 
     body.on('click', '#'+overlay_ID+', .'+imgcloned_class, function(e){
 
-        if(!items_cloned){
+        if(!clones_exist){
             $('.'+imgcloned_class).remove();
-            popup_trigger.each(function(){
-                $this = $(this);
-                create_popimg($this);
-                items_cloned = true;
+            popup_src.each(function(){
+                create_popimg($(this));
+                clones_exist = true;
+                clones = $('.'+imgcloned_class);
             });
+        } else {
+            clones.removeClass(show_class);
         }
 
         overlay.removeClass(show_class);
+
         e.stopPropagation();
         e.preventDefault();
     });
