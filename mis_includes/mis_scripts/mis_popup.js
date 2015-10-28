38f3d2bab296cda5ef7ef77d1d68ce79d009e765
Samuel Brynolf <samuel.brynolf@aventyret.com>
2015-10-28 09:32:03 +0100

    Working modal

--- mis_includes/mis_scripts/mis_popup.js
+++ mis_includes/mis_scripts/mis_popup.js
@@ -1,52 +1,114 @@
-// Modal image, Version: 1.0
-// ---------------------------------------------
+//// Modal image, Version: 1.0
+//// ---------------------------------------------
+//
+//(function($, document) {
+//
+//    var popup_src = $('.mis_popup'),
+//        body = $('body'),
+//        overlay_ID = 'mis_overlay',
+//        imgcloned_class = 'mis_is-cloned',
+//        new_sizes_attr = '100vw',
+//        show_class = 's-is-visible';
+//
+//    if(popup_src.length){
+//        // Create and append overlay
+//        var overlay = document.createElement('div');
+//        overlay.setAttribute('id', overlay_ID);
+//        document.body.appendChild(overlay);
+//        overlay = $('#'+overlay_ID); // Create jQuery object
+//
+//        // Create popup-clones
+//        popup_src.each(function(){
+//            var $this = $(this);
+//            var popped_id = $this.attr('data-misid');
+//
+//            $this.clone()
+//                .addClass(imgcloned_class)
+//                .attr('sizes', new_sizes_attr)
+//                .attr('id', popped_id).appendTo(body);
+//        });
+//        var popup_elems = $('.'+imgcloned_class).add(overlay);
+//    }
+//
+//    popup_src.on('click', function(e){
+//        var $this = $(this);
+//
+//        overlay.addClass(show_class);
+//        $('#'+ $this.attr('data-misid')).addClass(show_class);
+//        e.stopPropagation();
+//        e.preventDefault(e);
+//    });
+//
+//    popup_elems.on('click', function(e){
+//        popup_elems.removeClass(show_class);
+//        e.preventDefault();
+//    });
+//
+//})(jQuery, document);
+
+
+
+
+
+
+
+(function( $, document ) {
+    $.fn.mis_popup = function(options) {
+        var settings = $.extend({
+            body : $('body'),
+            overlay_ID : 'mis_overlay',
+            imgcloned_class : 'mis_is-cloned',
+            show_class : 's-is-visible',
+            pop_sizes : '100vw'
+        }, options );
+
+        var popup_src = this;
+
+        if(popup_src.length){
+            // Create and append overlay
+            var overlay = document.createElement('div');
+            overlay.setAttribute('id', settings.overlay_ID);
+            document.body.appendChild(overlay); // append later?
+            overlay = $('#'+settings.overlay_ID); // Create jQuery object
+
+            // Create popup-clones
+            popup_src.each(function(){
+                var $this = $(this);
+                var popped_id = $this.attr('data-misid');
+
+                $this.clone()
+                    .addClass(settings.imgcloned_class)
+                    .attr('sizes', settings.pop_sizes)
+                    .attr('id', popped_id).appendTo(settings.body);
+            });
+
+            var popup_elems = $('.'+settings.imgcloned_class).add(overlay);
+
+            popup_src.on('click', function(e){
+                var $this = $(this);
+
+                overlay.addClass(settings.show_class); // add below?
+                $('#'+ $this.attr('data-misid')).addClass(settings.show_class);
+                
+                e.stopPropagation();
+                e.preventDefault(e);
+            });
+
+            popup_elems.on('click', function(e){
+                popup_elems.removeClass(settings.show_class);
+                e.preventDefault();
+            });
+
+            return this;
+        }
+    };
+}( jQuery, document ));
+
 
 (function($, document) {
-
-    var popup_src = $('.mis_popup'),
-        body = $('body'),
-        overlay_ID = 'mis_overlay',
-        imgcloned_class = 'is-cloned',
-        new_sizes_attr = '100vw',
-        show_class = 's-is-visible';
-
-    if(popup_src.length){
-        // Create and append overlay
-        var overlay = document.createElement('div');
-        overlay.setAttribute('id', overlay_ID);
-        document.body.appendChild(overlay);
-        overlay = $('#'+overlay_ID); // Create jQuery object
-
-        // Create popup-clones
-        popup_src.each(function(){
-            var popped_img = $(this);
-            var popped_id = popped_img.attr('data-misid');
-
-            popped_img.clone()
-                .addClass(imgcloned_class)
-                .attr('sizes', new_sizes_attr)
-                .attr('id', popped_id).appendTo(body);
-        });
-        var clones = $('.'+imgcloned_class);
+    if($.fn.mis_popup) {
+        var popup_src = $('.mis_popup');
+        popup_src.mis_popup();
     }
-
-    popup_src.on('click', function(e){
-        var $this = $(this);
-        var popped_img = $('#'+ $this.attr('data-misid'));
-
-        overlay.addClass(show_class);
-        popped_img.addClass(show_class);
-
-        e.preventDefault(e);
-    });
-
-    //body.on('click', '#'+overlay_ID+', .'+imgcloned_class, function(e){
-    overlay.add(clones).on('click', function(e){
-        clones.removeClass(show_class);
-        overlay.removeClass(show_class);
-
-        e.stopPropagation();
-        e.preventDefault();
-    });
 
 })(jQuery, document);
